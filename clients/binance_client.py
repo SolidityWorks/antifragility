e7b2dea4cd99b960e33fb1e0014b3dfaa1b4f5a5
artemiev <mixartemev@gmail.com>
2022-11-06 11:34:44 +0300

    ads upd

--- clients/binance_client.py
+++ clients/binance_client.py
@@ -1,3 +1,4 @@
+from asyncio import run
 from time import time
 
 import aiohttp
@@ -6,12 +7,14 @@ from db.models import User
 
 gap = 0.01
 HOST = 'https://c2c.binance.com/'
-ADS_PTH = 'bapi/c2c/v2/friendly/c2c/adv/search'
-AD_PTH = 'bapi/c2c/v2/public/c2c/adv/selected-adv/'
-MY_ADS_PTH = 'bapi/c2c/v2/private/c2c/adv/list-by-page'
-PTS_PTH = 'bapi/c2c/v2/private/c2c/pay-method/user-paymethods'
-ORD_PTH = 'bapi/c2c/v2/private/c2c/order-match/order-list'
-ORD_ARCH_PTH = 'bapi/c2c/v1/private/c2c/order-match/order-list-archived-involved'
+ADS = 'bapi/c2c/v2/friendly/c2c/adv/search'
+AD = 'bapi/c2c/v2/public/c2c/adv/selected-adv/'
+AD_UPD = 'bapi/c2c/v3/private/c2c/adv/update'
+AD_UPD_ST = 'bapi/c2c/v2/private/c2c/adv/update-status'
+MY_ADS = 'bapi/c2c/v2/private/c2c/adv/list-by-page'
+PTS = 'bapi/c2c/v2/private/c2c/pay-method/user-paymethods'
+ORD = 'bapi/c2c/v2/private/c2c/order-match/order-list'
+ORD_ARCH = 'bapi/c2c/v1/private/c2c/order-match/order-list-archived-involved'
 BLNC_URL = 'https://www.binance.com/bapi/asset/v2/private/asset-service/wallet/balance?needBalanceDetail=true'
 
 
@@ -48,22 +51,22 @@ async def ping(user: User):
 
 
 async def get_my_pts(user: User):  # payment methods
-    res = await breq(PTS_PTH, user, {})
+    res = await breq(PTS, user, {})
     return res['data']
 
 
 async def get_my_ads(user: User):
-    res = await breq(MY_ADS_PTH, user, {"inDeal": 1, "rows": 50, "page": 1})
+    res = await breq(MY_ADS, user, {"inDeal": 1, "rows": 50, "page": 1})
     return res['data']
 
 
 async def act_orders(user: User):  # payment methods
-    res = await breq(ORD_PTH, user, {"page": 1, "rows": 20, "orderStatusList": [0, 1, 2, 3, 5]})
+    res = await breq(ORD, user, {"page": 1, "rows": 20, "orderStatusList": [0, 1, 2, 3, 5]})
     return res['data'], res['total']
 
 
 async def get_arch_orders(user: User, part: int = 0, page: int = 1):  # payment methods
-    res = await breq(ORD_ARCH_PTH, user, {"page": page or 1, "rows": 50, "startDate": int((time()-m6*(part+1))*1000), "endDate": int((time()-m6*part)*1000)})
+    res = await breq(ORD_ARCH, user, {"page": page or 1, "rows": 50, "startDate": int((time()-m6*(part+1))*1000), "endDate": int((time()-m6*part)*1000)})
     return res['data'], res['total']
 
 m6 = 60*60*24*30*6
@@ -87,10 +90,53 @@ async def get_ads(asset: str, cur: str, sell: int = 0, pts: [str] = None, rows: 
                "fiat": cur,
                # "transAmount": amount
                }
-    return await breq(ADS_PTH, None, payload)
+    return await breq(ADS, None, payload)
 
 
 async def get_ad(aid: str):
-    res = await breq(AD_PTH+aid, is_post=False)
+    res = await breq(AD+aid, is_post=False)
     return res.get('data')
 
+
+async def upd_ad():  # user, data: {}
+    user = await User.get(nickName='Deals')
+    data = {"asset": "RUB",
+            "fiatUnit": "RUB",
+            "priceType": 1,
+            # "priceScale": 2,
+            "advNo": 11419177391185489920,
+            "autoReplyMsg": "Взаимный лайк приветствуется:)\nЕсли усну, напишите сюда плиз или в tg: @ex212",
+            "initAmount": 50000,
+            "payTimeLimit": 30,
+            "price": 1.00,
+            "priceFloatingRatio": 100.12,
+            "minSingleTransAmount": 500,
+            "maxSingleTransAmount": 870000,
+            "remarks": "Оплачиваю быстро.\n",
+            "tradeMethods": [
+                {"identifier": "RaiffeisenBank"},
+                {"identifier": "RosBankNew"},
+                {"identifier": "TinkoffNew"},
+                {"identifier": "QIWI"},
+                {"identifier": "YandexMoneyNew"}],
+            "tradeType": "BUY",
+            "launchCountry": []  # "AE", "TR"
+            }
+    res = await breq(AD_UPD, user, data)
+    return res.get('data')
+
+
+async def upd_ad_status(aid: int, pub: bool = True):  # user, data: {}
+    user = await User.get(nickName='Deals')
+    data = {"advNos": [f"1{aid}"], "advStatus": int(pub)}
+    res = await breq(AD_UPD_ST, user, data)
+    return res.get('data')
+
+
+if __name__ == "__main__":
+    try:
+        from loader import cns
+        # run(upd_ad())
+        run(upd_ad_status(1419177124233637888))
+    except KeyboardInterrupt:
+        print('Stopped.')
